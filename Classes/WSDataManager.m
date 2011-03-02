28af295ffd350eaf30dcb5270b06a21a226364c0
Tom Hartley <tomhartley@rocketmail.com>
2011-03-02 22:45:00 +0000

    Added badging of app with current preps, also tried to fix iOS 4.0 bug

--- Classes/WSDataManager.m
+++ Classes/WSDataManager.m
@@ -42,6 +42,7 @@ static WSDataManager *sharedInstance  = nil;
 	[currentPrep release];
 	currentPrep=prep;
 	[currentPrep retain];
+	[[UIApplication sharedApplication] setApplicationIconBadgeNumber:[currentPrep count]];
 	[[NSNotificationCenter defaultCenter] postNotificationName:@"WSPrepUpdatedNotification" object:nil];
 }
 
