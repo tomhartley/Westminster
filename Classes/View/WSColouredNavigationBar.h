313c6cf19f8625adb4f2839d35fcaf92976fbbfb
Tom Hartley <tomhartley@rocketmail.com>
2011-03-17 19:12:02 +0000

    Fixed a pile of bugs: sign in button, refactored copypastacode, added multitasking (alpha quality at the moment)

---
+++ Classes/View/WSColouredNavigationBar.h
@@ -0,0 +1,16 @@
+//
+//  WSColouredNavigationBar.h
+//  Westminster
+//
+//  Created by Tom Hartley on 17/03/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface WSColouredNavigationBar : UIView {
+    
+}
+
+@end
---
+++ Classes/View/WSColouredNavigationBar.m
@@ -0,0 +1,37 @@
+//
+//  WSColouredNavigationBar.m
+//  Westminster
+//
+//  Created by Tom Hartley on 17/03/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "WSColouredNavigationBar.h"
+
+
+@implementation WSColouredNavigationBar
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
