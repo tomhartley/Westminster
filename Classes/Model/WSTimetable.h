c1942f5855f4b3e5426bb9d134c6da86599c5973
administrator <admin@IT-Department-Mac.local>
2011-05-06 16:12:31 +0100

    Added some minor timetable features...

---
+++ Classes/Model/WSTimetable.h
@@ -0,0 +1,20 @@
+//
+//  WSTimetable.h
+//  Westminster
+//
+//  Created by administrator on 06/05/2011.
+//  Copyright 2011 Westminster School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef enum {
+    WSDayFormatShort,
+    WSDayFormatLong
+} WSDayFormat;
+
+
+@interface WSTimetable : NSObject {
+}
+
+@end
